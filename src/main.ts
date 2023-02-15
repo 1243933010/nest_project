23ff50e2f0395ba5cad6907e5ebc861b57585098
ysys <1243933010@qq.com>
2023-02-15 17:57:41 +0800

    添加验证码插件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {VersioningType} from '@nestjs/common'
+import * as session from 'express-session'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type:VersioningType.URI
   })
+  app.use(session({secret:'yueshaochong',rolling:true,name:'yueshaochong.sid',cookie:{maxAge:9999999}}));
   await app.listen(3000);
 }
 bootstrap();
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete,Request,Query,Headers,HttpCode } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as svgCaptcha from 'svg-captcha';
 
 // @Controller('user')
 @Controller({
@@ -13,17 +14,40 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body('name') createUserDto: CreateUserDto) {
+    console.log(createUserDto);
     return this.userService.create(createUserDto);
   }
 
+  @Get('verification')
+  verification(){
+    const Captcha = svgCaptcha.create({
+      size:4,
+      fontSize:50,
+      width:100,
+      height:34,
+      background:'#cc9966'
+    })
+    return {
+      code:'200',
+      data:Captcha
+    }
+  }
+
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  findAll(@Query() query) {
+    console.log(query);
+    return {
+      code:'200',
+      message:query.name
+    }
+    // return this.userService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  // @HttpCode(404)
+  findOne(@Param('id') id: string ,@Headers('cookie') headers:any) {
+    console.log(id,headers)
     return this.userService.findOne(+id);
   }
 
